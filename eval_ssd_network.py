4d2789666def450a04cc2860ac9b4a92f388a69c
Seong Hoon Seo <andyseo247@gmail.com>
2018-02-13 16:04:42 +0900

    fix typo

--- eval_ssd_network.py
+++ eval_ssd_network.py
@@ -339,7 +339,7 @@ def main(_):
                 checkpoint_dir=checkpoint_path,
                 logdir=FLAGS.eval_dir,
                 num_evals=num_batches,
-                eval_op=flatten(list(name_to_updates.values())),
+                eval_op=flatten(list(names_to_updates.values())),
                 variables_to_restore=variables_to_restore,
                 eval_interval_secs=60,
                 max_number_of_evaluations=np.inf,
